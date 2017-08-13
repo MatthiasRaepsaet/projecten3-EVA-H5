2a7992c8bbeb434e7cfa0d0b4f252a5e462324bf
chaixdev <bhagwat.chaitanya@gmail.com>
2017-08-13 07:08:38 +0200

    domain voor challenges + aanzet ui fragments

---
+++ Android/app/src/main/java/be/hogent/Eva2017g5/EVAH5/domain/Challenge.java
@@ -0,0 +1,8 @@
+package be.hogent.Eva2017g5.EVAH5.domain;
+
+/**
+ * Created by chaitanya on 8/13/17.
+ */
+
+public abstract class Challenge {
+}
---
+++ Android/app/src/main/java/be/hogent/Eva2017g5/EVAH5/ui/HomeActivity.java
@@ -0,0 +1,15 @@
+package be.hogent.Eva2017g5.EVAH5.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import be.hogent.Eva2017g5.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
