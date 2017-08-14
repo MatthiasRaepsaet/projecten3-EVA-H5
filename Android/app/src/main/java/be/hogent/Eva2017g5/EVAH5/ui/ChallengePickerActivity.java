54640c07cce36c56f29cb754ee48e55cf3cad524
chaixdev <bhagwat.chaitanya@gmail.com>
2017-08-14 17:01:53 +0200

    launch 2de activity + toggle buttons state

---
+++ Android/app/src/main/java/be/hogent/Eva2017g5/EVAH5/ui/ChallengePickerActivity.java
@@ -0,0 +1,15 @@
+package be.hogent.Eva2017g5.EVAH5.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import be.hogent.Eva2017g5.R;
+
+public class ChallengePicker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_challenge_picker);
+    }
+}
