3b145bffdb07a84e21b094925e77d616fbe9e05e
MatthiasRaepsaet <matthy.raepsaet@hotmail.com>
2017-08-16 23:23:31 +0200

    tweaks2

--- Backend/BackendEvaProjecten3/src/main/java/hello/Application.java
+++ Backend/BackendEvaProjecten3/src/main/java/hello/Application.java
@@ -1,14 +1,23 @@
 package hello;
 
+import hello.controllers.RecipeController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class Application {
+
+    /*@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }*/
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
