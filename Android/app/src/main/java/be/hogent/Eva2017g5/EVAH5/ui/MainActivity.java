92e4d4b0f520b17fa107c4a08cf23b4cd94dfdca
DESKTOP-O5ESE9D\sofie <sofie@delaeter.be>
2017-08-18 00:44:54 +0200

    navigatie verbeterd

--- Android/app/src/main/java/be/hogent/Eva2017g5/EVAH5/ui/MainActivity.java
+++ Android/app/src/main/java/be/hogent/Eva2017g5/EVAH5/ui/MainActivity.java
@@ -1,11 +1,13 @@
 package be.hogent.Eva2017g5.EVAH5.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -15,272 +17,97 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import  be.hogent.Eva2017g5.EVAH5.ui.fragments.*;
 import be.hogent.Eva2017g5.R;
 
-public class MainActivity extends AppCompatActivity {
-    private NavigationView navigationView;
-    private DrawerLayout drawer;
-    private View navHeader;
-    private TextView txtName;
-    private Toolbar toolbar;
-    private FloatingActionButton fab;
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private FrameLayout contentPanelFrame;
+    private FrameLayout contentFrame;
+    private Menu menu;
 
     // index to identify current nav menu item
     public static int navItemIndex = 0;
 
-    // tags used to attach the fragments
-    private static final String TAG_EVA = "Over EVA";
-    private static final String TAG_VEGVSVEG = "vegvsveg";
-    private static final String TAG_CHALLENGES = "challenges";
-    private static final String TAG_UITLEG = "uitleg";
-    private static final String TAG_PROFIEL = "profiel";
-    public static String CURRENT_TAG = TAG_EVA;
-
-    // toolbar titles respected to selected nav menu item
-    private String[] activityTitles;
-
-    // flag to load home fragment when user presses back key
-    private boolean shouldLoadHomeFragOnBackPress = true;
-    private Handler mHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Bundle bundle = this.getIntent().getExtras();
         setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        contentFrame = (FrameLayout) findViewById(R.id.content_navigation);
+        contentPanelFrame = (FrameLayout) findViewById(R.id.content_minor_panel);
+
+        fragmentManager.beginTransaction().replace(R.id.content_navigation,new OverEVAFragment()).commit();
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        mHandler = new Handler();
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
 
-        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        navigationView = (NavigationView) findViewById(R.id.nav_view);
-        fab = (FloatingActionButton) findViewById(R.id.fab);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        View header = navigationView.getHeaderView(0);
+        TextView username = (TextView) header.findViewById(R.id.username);
+        username.setText(bundle.getString("USERNAME"));
 
-        // Navigation view header
-        navHeader = navigationView.getHeaderView(0);
-        txtName = (TextView) navHeader.findViewById(R.id.username);
-
-        // load toolbar titles from string resources
-        activityTitles = getResources().getStringArray(R.array.nav_item_activity_titles);
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        // load nav menu header data
-        loadNavHeader();
-
-        // initializing navigation menu
-        setUpNavigationView();
-
-        if (savedInstanceState == null) {
-            navItemIndex = 0;
-            CURRENT_TAG = TAG_EVA;
-            loadHomeFragment();
-        }
-    }
-
-    /***
-     * Load navigation menu header information
-     * like background image, username
-     */
-
-    private void loadNavHeader() {
-        txtName.setText("Hier moet hij dan de username van de ingelogde user tonen");
-    }
-
-    /***
-     * Returns respected fragment that user
-     * selected from navigation menu
-     */
-    private void loadHomeFragment() {
-        // selecting appropriate nav menu item
-        selectNavMenu();
-
-        // set toolbar title
-        setToolbarTitle();
-
-        // if user select the current navigation menu again, don't do anything
-        // just close the navigation drawer
-        if (getSupportFragmentManager().findFragmentByTag(CURRENT_TAG) != null) {
-            drawer.closeDrawers();
-
-            // show or hide the fab button
-            toggleFab();
-            return;
-        }
-
-        // Sometimes, when fragment has huge data, screen seems hanging
-        // when switching between navigation menus
-        // So using runnable, the fragment is loaded with cross fade effect
-        // This effect can be seen in GMail app
-        Runnable mPendingRunnable = new Runnable() {
-            @Override
-            public void run() {
-                // update the main content by replacing fragments
-                Fragment fragment = getHomeFragment();
-                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-                fragmentTransaction.setCustomAnimations(android.R.anim.fade_in,
-                        android.R.anim.fade_out);
-                fragmentTransaction.replace(R.id.frame, fragment, CURRENT_TAG);
-                fragmentTransaction.commitAllowingStateLoss();
-            }
-        };
-
-        // If mPendingRunnable is not null, then add to the message queue
-        if (mPendingRunnable != null) {
-            mHandler.post(mPendingRunnable);
-        }
-
-        // show or hide the fab button
-        toggleFab();
-
-        //Closing drawer on item click
-        drawer.closeDrawers();
-
-        // refresh toolbar menu
-        invalidateOptionsMenu();
-    }
-
-
-    private Fragment getHomeFragment() {
-        switch (navItemIndex) {
-            case 0:
-                // over EVA
-                OverEVAFragment evaFragment = new OverEVAFragment();
-                return evaFragment;
-            case 1:
-                // Vegetarisme vs veganisme
-                VegvsVegFragment vegFragment = new VegvsVegFragment();
-                return vegFragment;
-            case 2:
-                // challenges
-                ChallengesFragment challengesFragment = new ChallengesFragment();
-                return challengesFragment;
-            case 3:
-                // uitleg
-                UitlegFragment uitlegFragment = new UitlegFragment();
-                return uitlegFragment;
-
-            case 4:
-                // profiel
-                ProfileFragment profielFragment = new ProfileFragment();
-                return profielFragment;
-            default:
-                return new OverEVAFragment();
-        }
-    }
-
-    private void setToolbarTitle() {
-        getSupportActionBar().setTitle(activityTitles[navItemIndex]);
-    }
-
-    private void selectNavMenu() {
-        navigationView.getMenu().getItem(navItemIndex).setChecked(true);
-    }
-
-    private void setUpNavigationView() {
-        //Setting Navigation View Item Selected Listener to handle the item click of the navigation menu
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-
-            // This method will trigger on item Click of navigation menu
-            @Override
-            public boolean onNavigationItemSelected(MenuItem menuItem) {
-
-                //Check to see which item was being clicked and perform appropriate action
-                switch (menuItem.getItemId()) {
-                    //Replacing the main content with ContentFragment Which is our Inbox View;
-                    case R.id.nav_overEVA:
-                        navItemIndex = 0;
-                        CURRENT_TAG = TAG_EVA;
-                        break;
-                    case R.id.nav_vegvsVeg:
-                        navItemIndex = 1;
-                        CURRENT_TAG = TAG_VEGVSVEG;
-                        break;
-                    case R.id.nav_challenges:
-                        navItemIndex = 2;
-                        CURRENT_TAG = TAG_CHALLENGES;
-                        break;
-                    case R.id.nav_uitleg:
-                        navItemIndex = 3;
-                        CURRENT_TAG = TAG_UITLEG;
-                        break;
-                    case R.id.nav_profiel:
-                        navItemIndex = 4;
-                        CURRENT_TAG = TAG_PROFIEL;
-                        break;
-                    default:
-                        navItemIndex = 0;
-                }
-
-                //Checking if the item is in checked state or not, if not make it in checked state
-                if (menuItem.isChecked()) {
-                    menuItem.setChecked(false);
-                } else {
-                    menuItem.setChecked(true);
-                }
-                menuItem.setChecked(true);
-
-                loadHomeFragment();
-
-                return true;
-            }
-        });
-
-
-        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.openDrawer, R.string.closeDrawer) {
-
-            @Override
-            public void onDrawerClosed(View drawerView) {
-                // Code here will be triggered once the drawer closes as we dont want anything to happen so we leave this blank
-                super.onDrawerClosed(drawerView);
-            }
-
-            @Override
-            public void onDrawerOpened(View drawerView) {
-                // Code here will be triggered once the drawer open as we dont want anything to happen so we leave this blank
-                super.onDrawerOpened(drawerView);
-            }
-        };
-
-        //Setting the actionbarToggle to drawer layout
-        drawer.setDrawerListener(actionBarDrawerToggle);
-
-        //calling sync state is necessary or else your hamburger icon wont show up
-        actionBarDrawerToggle.syncState();
     }
 
     @Override
     public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawers();
-            return;
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
-
-        // This code loads home fragment when back key is pressed
-        // when user is in other fragment than home
-        if (shouldLoadHomeFragOnBackPress) {
-            // checking if user is on other navigation menu
-            // rather than home
-            if (navItemIndex != 0) {
-                navItemIndex = 0;
-                CURRENT_TAG = TAG_EVA;
-                loadHomeFragment();
-                return;
-            }
-        }
-
-        super.onBackPressed();
     }
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        Fragment fragment = null;
+        Bundle bundle = new Bundle();
+        switch (id){
+            case R.id.nav_challenges: fragment = new ChallengesFragment();
+                break;
+            case R.id.nav_overEVA: fragment = new OverEVAFragment();
+                break;
+            case R.id.nav_vegvsVeg: fragment = new VegvsVegFragment();
+                break;
+            case R.id.nav_recipes: fragment = new RecipesFragment();
+                break;
+            case R.id.nav_logout:
+                Intent navIntent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(navIntent);
+                return true;
+        }
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.content_navigation, fragment).commit();
+
+        item.setChecked(true);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -295,28 +122,6 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_logout) {
-            Toast.makeText(getApplicationContext(), "Logout user!", Toast.LENGTH_LONG).show();
-            return true;
-        }
 
 
-        return super.onOptionsItemSelected(item);
-    }
-
-    // show or hide the fab
-    private void toggleFab() {
-        if (navItemIndex == 0)
-            fab.show();
-        else
-            fab.hide();
-    }
 }
---
+++ Android/app/src/main/java/be/hogent/Eva2017g5/EVAH5/ui/fragments/RecipeAdapter.java
@@ -0,0 +1,128 @@
+package be.hogent.Eva2017g5.EVAH5.ui.fragments;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import be.hogent.Eva2017g5.R;
+import be.hogent.Eva2017g5.EVAH5.rest.Recipe;
+import be.hogent.Eva2017g5.EVAH5.ui.MainActivity;
+
+/**
+ * Created by sofie.
+ */
+
+class RecipeAdapter extends BaseAdapter implements Filterable {
+    private ArrayList<Recipe> rOriginal, rDisplayed;
+    LayoutInflater inflater;
+
+    public RecipeAdapter(Context context, ArrayList<Recipe> recipeArrayList){
+        this.rDisplayed = recipeArrayList;
+        this.rOriginal = recipeArrayList;
+        inflater= LayoutInflater.from(context);
+    }
+
+
+    @Override
+    public int getCount() {
+        return rDisplayed.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    private class ViewHolder {
+        LinearLayout llContainer;
+        TextView rTitel,rDescription, rVotes;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+
+        ViewHolder holder = null;
+
+        if (convertView == null) {
+
+            holder = new ViewHolder();
+            convertView = inflater.inflate(R.layout.row, null);
+            holder.llContainer = (LinearLayout)convertView.findViewById(R.id.llContainer);
+            holder.rTitel = (TextView) convertView.findViewById(R.id.rTitel);
+            holder.rDescription = (TextView) convertView.findViewById(R.id.rDescription);
+            holder.rVotes = (TextView) convertView.findViewById(R.id.rVotes);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        holder.rTitel.setText(rDisplayed.get(position).getTitle());
+        holder.rDescription.setText(rDisplayed.get(position).getDescription());
+        holder.rVotes.setText(rDisplayed.get(position).getVotes());
+
+        return convertView;
+    }
+
+    @Override
+    public Filter getFilter() {
+        Filter filter = new Filter() {
+
+            @SuppressWarnings("unchecked")
+            @Override
+            protected void publishResults(CharSequence constraint,FilterResults results) {
+
+                rDisplayed = (ArrayList<Recipe>) results.values; // has the filtered values
+                notifyDataSetChanged();  // notifies the data with new filtered values
+            }
+
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                FilterResults results = new FilterResults();        // Holds the results of a filtering operation in values
+                ArrayList<Recipe> FilteredArrList = new ArrayList<Recipe>();
+
+                if (rOriginal == null) {
+                    rOriginal = new ArrayList<Recipe>(rDisplayed); // saves the original data in mOriginalValues
+                }
+
+                /********
+                 *
+                 *  If constraint(CharSequence that is received) is null returns the mOriginalValues(Original) values
+                 *  else does the Filtering and returns FilteredArrList(Filtered)
+                 *
+                 ********/
+                if (constraint == null || constraint.length() == 0) {
+
+                    // set the Original result to return
+                    results.count = rOriginal.size();
+                    results.values = rOriginal;
+                } else {
+                    constraint = constraint.toString().toLowerCase();
+                    for (int i = 0; i < rOriginal.size(); i++) {
+                        String data = rOriginal.get(i).getTitle();
+                        if (data.toLowerCase().startsWith(constraint.toString())) {
+                            FilteredArrList.add(new Recipe(rOriginal.get(i).getTitle(),rOriginal.get(i).getDescription(), rOriginal.get(i).getVotes()));
+                        }
+                    }
+                    // set the Filtered result to return
+                    results.count = FilteredArrList.size();
+                    results.values = FilteredArrList;
+                }
+                return results;
+            }
+        };
+        return filter;
+    }
+}
+
